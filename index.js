7f2ee8151ced0d2849de53a409595ecd7ff1910d
Egor9000662 <egor1908608@gmail.com>
2021-02-19 20:11:37 +0300

    added image

--- index.js
+++ index.js
@@ -1,57 +1,52 @@
-
 const loremText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia excepturi commodi impedit, possimus nulla vitae et ex. Quam nisi minima cum natus rem esse voluptatum laborum eveniet sequi. Quo, quibusdam.';
 const sections = [
     { type: 'title', value: 'Hello world!' },
     { type: 'text', value: loremText },
-    { type: 'columns', value: [ loremText, loremText, loremText ] }
+    { type: 'columns', value: [ loremText, loremText, loremText ] },
+    { type: 'image', value: './assets/profile.jpg'}
 ];
 
 const $content = document.querySelector('#cv');
 
-sections.forEach(section => {
-    let html = '';
+function row(content) {
+    return `<section class="row">${content}</section>`;
+}
 
-    if (section.type === 'title') {
-        html = title(section);
-    } else if (section.type === 'text') {
-        html = text(section);
-    } else if (section.type === 'columns') {
-        html = columns(section);
-    }
-
-    $content.insertAdjacentHTML('beforeend', html)
-})
+function col(content) {
+    return `<div class="col-sm">${content}</div>`;
+}
 
 function title(section) {
-    return  `
-    <section class="row">
-        <div class="col-sm">
-            <h2>${section.value}</h2>
-        </div>
-    </section>
-`;
+    return row(col(`<h2>${section.value}</h2>`));
 }
 
 function text(section) {
-    return `
-    <section class="row">
-        <div class="col-sm">
-            <p>${section.value}</p>
-        </div>
-    </section>
-    `;
+    return row(col(`<p>${section.value}</p>`));
 }
 
 function columns(section) {
     const html = section.value.map(
         item => {
-            return `<div class="col-sm"><p>${item}</p></div>`
+            return col(`<p>${item}</p>`)
         }
     );
 
-    return `
-    <section class="row">
-        ${html.join('')}
-    </section>
-    `;
+    return row(html.join(''));
 }
+
+function image(section) {
+    return row(col(`<img src="${section.value}" />`));
+} 
+
+const templates = {
+   title: title,
+   text: text,
+   columns: columns,
+   image: image,
+};
+
+sections.forEach(section => {
+    const template = templates[section.type];
+    const html = template(section);
+    $content.insertAdjacentHTML('beforeend', html)
+})
